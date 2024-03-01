963689e30d105ce8d96ebd6c3338f91bee216688
vicky wong <vicky.boci@gmail.com>
2024-03-02 03:23:09 +0800

    updated the first draft of those jsx files

---
+++ starter/reactpractice/src/components/APP/App.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import Home from './Home.jsx';
+import Contact from './components/Contact/Contact.jsx'
+import Footer from './components/Footer/Footer.jsx';
+import Navbar from './components/Navbar/Narbar.jsx';
+import Project from './components/Project/Project.jsx';
+import ProjectGarllery from './components/ProjectGarllery/ProjectGarllery.jsx';
+
+
+
+
+
+
+
+
+
+
+
+export default App;
---
+++ starter/reactpractice/src/components/ProjectGallery/ProjectGallery.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./ProjectGarllery.css";
+
+function ProjectGarllery() {
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img
+          alt="Gallery"
+          src="https://vignette.wikia.nocookie.net/spongebobgalaxy/images/0/07/SpongeBob_SquarePants.png/revision/latest?cb=20171228024014"
+        />
+      </div>
+      <div className="content">
+        <ul>
+          <li>
+            <strong>Name:</strong> Sample
+          </li>
+          <li>
+            <strong>Occupation:</strong> Fry Cook
+          </li>
+          <li>
+            <strong>Location:</strong> A Pinapple Under the Sea
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectGarllery;
