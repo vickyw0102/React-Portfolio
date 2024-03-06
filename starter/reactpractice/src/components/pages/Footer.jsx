5da1b93a90714e40adf8a3198056050441d02c3a
vicky wong <vicky.boci@gmail.com>
2024-03-06 23:00:03 +0800

    updated the images files and created footer and navbar; 2nd draft of Home, Contact and Project pages

---
+++ starter/reactpractice/src/components/pages/Footer.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import '../stylesheet/footer.css'
+
+function Footer() {
+    return (
+        <>
+            <footer>
+                <section id="ContactInfo">
+                    <div className="footer">
+                    
+                        <div className="sectionHeading">
+                            <h5 className="display-6 text-center">Contact Information</h5>
+                        </div>
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-4 text-left">
+                                    <h4>📞 Phone: (+44) 077xx xxxxxx</h4>
+                                    <h4>🏡Address: Rmxx, 1 Street, Birmingham B35xxx</h4>
+                                    
+                                </div>
+                                
+                                <div className="col-md-4 text-center">
+                                <h4>🎂Date of birth: 1/ Jan/1999 </h4>
+                                <h4>📧Email:<a href= "mailto:abc@gmailcom"> abv@gmail.com</a> </h4>
+
+                                </div>
+                               
+                            </div>
+                        </div>
+                    </div>  
+                </section>
+            </footer>
+        </>
+);
+}
+
+export default Footer;
--- starter/reactpractice/src/components/pages/Header.jsx
+++ starter/reactpractice/src/components/pages/Header.jsx
@@ -1,71 +1,70 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import '../stylesheet/Header.css';
+import Home from './Home';
+import Project from './Project';
 
 
 function Header() {
 return (
-  <>
-    
-    
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid">
-                    <div className="Logo">
-                            <img src='../public/vite.svg' className="App-logo" alt="logo>"></img>
+    <>
+        <section> 
+            <nav className="navbar navbar-expand-lg     cbg-body-tertiary CustomNav">
+                <div className="container-fluid">
 
-                    <ul className="nav nav-tabs">
+                        <ul className="nav nav-tabs">
+
+                            <li className="nav-item">
+                                <NavLink
+                                to="/"
+                                end
+                                className={({ isActive }) =>
+                                    isActive ? 'nav-link active' : 'nav-link'
+                                }
+                                >
+                                Home
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink
+                                to="Contact"
+                                className={({ isActive }) =>
+                                    isActive ? 'nav-link active' : 'nav-link'
+                                }
+                                >
+                                Contact
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink
+                                to="Project"
+                                className={({ isActive }) =>
+                                    isActive ? 'nav-link active' : 'nav-link'
+                                }
+                                >
+                                Project
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink
+                                to="Resume"
+                                end
+                                className={({ isActive }) =>
+                                    isActive ? 'nav-link active' : 'nav-link'
+                                }
+                                >
+                                Resume
+                                </NavLink>
+                            </li>
+                        </ul>
                         
-                        
-                        <li className="nav-item">
-                            <NavLink
-                            to="/"
-                            end
-                            className={({ isActive }) =>
-                                isActive ? 'nav-link active' : 'nav-link'
-                            }
-                            >
-                            Home
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink
-                            to="Contact"
-                            className={({ isActive }) =>
-                                isActive ? 'nav-link active' : 'nav-link'
-                            }
-                            >
-                            Contact
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink
-                            to="Project"
-                            className={({ isActive }) =>
-                                isActive ? 'nav-link active' : 'nav-link'
-                            }
-                            >
-                            Project
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink
-                            to="Resume"
-                            end
-                            className={({ isActive }) =>
-                                isActive ? 'nav-link active' : 'nav-link'
-                            }
-                            >
-                            Resume
-                            </NavLink>
-                        </li>
-                    </ul>
-                    </div>
                 </div>
+            </nav>
             
-    
-        </nav>
-    
-  </>
+        </section>                    
+        
+        
+    </>
 );
 }
 
