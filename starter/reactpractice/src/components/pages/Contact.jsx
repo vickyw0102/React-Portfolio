bad6972d559efc97d58258c77191d1822968fe60
vicky wong <vicky.boci@gmail.com>
2024-03-11 04:40:59 +0800

    Completed the contact page and contact form

--- starter/reactpractice/src/components/pages/Contact.jsx
+++ starter/reactpractice/src/components/pages/Contact.jsx
@@ -12,33 +12,15 @@ function Contact() {
 
         <div class="p-5 mb-4 bg-body-tertiary rounded-3 Background">
          <div class="container-fluid py-5 jumboCopy">
-          <h1 class="display-5 fw-bold">Hi, I'm Vicky Wong</h1>
-          <p class="col-md-12 fs-4">Front-end Website developer</p>
+          <h1 class="display-5 fw-bold">Know more about me </h1>
+          <p class="fs-4">Front-end Website developer</p>
           </div>
-        </div>
-        
-        <h1>Contact Page1</h1>
-        <p>
-          Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-          molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-          magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-          efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-          mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-          posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-          faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-          ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-          dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-          conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-          rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-        </p>
         <ContactForm/>
+        </div>
+
+      
         
-        <Link to="learn" role="button" className="btn btn-link">
-          Learn More
-        </Link>
-        <Link to="contact" role="button" className="btn btn-link">
-          Learn Less
-        </Link>
+
      
       </div>
     </>
