64c01fa5bbb64d0a37e99a5eb46a8a87485696b8
vicky wong <vicky.boci@gmail.com>
2024-03-11 18:51:36 +0800

    deleted some pic and uploaded the project's pic

--- starter/reactpractice/src/components/pages/Project.jsx
+++ starter/reactpractice/src/components/pages/Project.jsx
@@ -8,26 +8,28 @@ import ProjectList from "../data/ProjectList.json"
 function Project() {
   return (
     <>
-      <div>
-        <span> <h1> My Projects </h1></span> 
-        <p>
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-          in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-        </p>
-        <p>
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-          in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-        </p>
+      
+        <div className="projectList">
+          
+          <div class="p-5 mb-4 bg-body-tertiary rounded-3 Background">
+          <div class="container-fluid py-5 jumboCopy">
+            <h1 class="display-5 fw-bold">Hi, I'm Vicky Wong</h1>
+
+          </div>
+
+        </div>
+
       </div>
       <div>
+        {/*search button}
+        {/* <div className="input-box">
+
+          <input type="search" 
+          name="search-form"
+          className="search-input"
+          onChange={(e)= setSearchQuery(e.target.value)}
+          placeholder="Search Project"/>
+        </div> */}
       <Wrapper>
       {/* <Title>Friends List</Title> */}
       <Cards
