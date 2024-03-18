96142b8d81cfbf5840c4e6c012ce4bf5fdee0fe6
vicky wong <vicky.boci@gmail.com>
2024-03-18 16:59:10 +0800

    updated the images in header

--- src/components/pages/Header.jsx
+++ src/components/pages/Header.jsx
@@ -12,7 +12,7 @@ return (
     <>
         <nav className="navbar navbar-expand-lg bg-body-tertiary customNav">
             <div className="container-fluid">
-                <a className="navbar-brand "><img src="/images/logo4.png" /> Vicky's Portfolio </a>
+                <a className="navbar-brand "><img src="./React-Portfolio/images/logo4.png" /> Vicky's Portfolio </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
