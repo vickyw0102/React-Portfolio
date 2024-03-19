9dad5accaf575a8b22d6f18ac480609ca6cc6e7d
vicky wong <vicky.boci@gmail.com>
2024-03-19 18:35:52 +0800

    changed the Navlink of Home page

--- src/components/pages/Header.jsx
+++ src/components/pages/Header.jsx
@@ -17,7 +17,7 @@ return (
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
                         <li className="nav-item">
-                            <NavLink to="React-Portfolio"end>
+                            <NavLink to="/"end>
                             Home
                             </NavLink>
                         </li>
