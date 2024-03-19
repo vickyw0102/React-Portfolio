2453c7bb932332f6ec4e6e60892b3414b8944bc5
Vicky Wong <vicky.boci@gmail.com>
2024-03-19 18:56:10 +0800

    Update Header.jsx Navlink to React-Portfolio for Home page

--- src/components/pages/Header.jsx
+++ src/components/pages/Header.jsx
@@ -17,7 +17,7 @@ return (
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
                         <li className="nav-item">
-                            <NavLink to="/"end>
+                            <NavLink to="/React-Portfolio"end>
                             Home
                             </NavLink>
                         </li>
